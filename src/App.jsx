3b290d8894a381ce00bae5d3d52da675d9f019c1
James1913 <elloco1913@hotmail.com>
2024-03-11 17:15:34 -0500

    one commit}

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <h1>I'am James Bolaños😉❤️</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
